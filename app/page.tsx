90d6eb2f5c9150f4bda6d23bd8c16e6aa85b6545
Afrouz8789 <afrouzashar@gmail.com>
2024-04-06 14:53:47 +0530

    added base section

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,40 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="px">
-      home
-    </h1>
+    <>
+      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              Shopping Starts Here:
+              <Image
+                src="assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+            <h1 className="head-text">
+              Unleash the power of
+              <span className="text-primary">PriceWise</span>
+            </h1>
+            <p className="mt-6">
+              Powerful, self-serve product and growth analytics to help you
+              convert, engage, and retain more.
+            </p>
+            Search bar
+          </div>
+          Hero carousel
+        </div>
+      </section>
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {["Apple Iphone 15", "Book", "Sneakers"].map((product, index) => (
+            <div>{product}</div>
+          ))}
+        </div>
+      </section>
+    </>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,28 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
+const NavIcons=[{src:'/assets/icons/search.svg',alt:'search'},
+{src:'/assets/icons/black-heart.svg',alt:'heart'},
+{src:'/assets/icons/user.svg',alt:'user'}
+]
 const Navbar = () => {
   return (
-    <div className='px-10'>Navbar</div>
+    <header className='w-full'>
+      <nav className='nav'>
+        <Link href={'/'} className='flex items-center gap-1'>
+          <Image src='/assets/icons/logo.svg' width={27} height={27} alt='logo'/>
+          <p className='nav-logo'>
+            Price<span className='text-primary'>Wise</span>
+          </p>
+        </Link>
+        <div className='flex items-center gap-5'>
+          {
+            NavIcons.map((icon,index) => <Image src={icon.src} alt={icon.alt} width={28} height={28} key={index} className='object-contain'/>)
+          }
+        </div>
+      </nav>
+    </header>
   )
 }
 
