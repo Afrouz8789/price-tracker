0e295d754c5520a9700c5bc597c644ecdcb60040
Afrouz8789 <afrouzashar@gmail.com>
2024-04-06 20:59:08 +0530

    added her carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import HeroCarousel from "@/components/HeroCarousel";
+import Searchbar from "@/components/Searchbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+      <section className="px-6 md:px-20 py-24">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
@@ -23,9 +25,9 @@ export default function Home() {
               Powerful, self-serve product and growth analytics to help you
               convert, engage, and retain more.
             </p>
-            Search bar
+            <Searchbar />
           </div>
-          Hero carousel
+          <HeroCarousel />
         </div>
       </section>
       <section className="trending-section">
